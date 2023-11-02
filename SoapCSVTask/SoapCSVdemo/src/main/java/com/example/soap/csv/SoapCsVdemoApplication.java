63ac54bdb8faddecadd1fb0fa9a992da851c1f8e
Yomnaelsoufy <yomnaelsoufy45@gmail.com>
2023-11-02 21:37:46 +0200

    soapcsv service

---
+++ SoapCSVTask/SoapCSVdemo/src/main/java/com/example/soap/csv/SoapCsVdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.soap.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCsVdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCsVdemoApplication.class, args);
+	}
+
+}
---
+++ SoapCSVTask/SoapCSVdemo/src/test/java/com/example/soap/csv/SoapCsVdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soap.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCsVdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
