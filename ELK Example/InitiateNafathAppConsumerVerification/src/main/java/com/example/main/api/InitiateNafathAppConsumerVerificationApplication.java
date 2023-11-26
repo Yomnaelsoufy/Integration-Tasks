bf2ea95a861857e28404d408226eb778398f0751
Yomnaelsoufy <yomnaelsoufy45@gmail.com>
2023-11-26 09:25:32 +0200

    ELKExample first version

---
+++ ELK Example/InitiateNafathAppConsumerVerification/src/main/java/com/example/main/api/InitiateNafathAppConsumerVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.main.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiateNafathAppConsumerVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitiateNafathAppConsumerVerificationApplication.class, args);
+	}
+
+}
---
+++ ELK Example/InitiateNafathAppConsumerVerification/src/main/java/com/example/main/api/router/FirstServiceRouter.java
@@ -0,0 +1,40 @@
+package com.example.main.api.router;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+import org.apache.camel.component.es.ElasticsearchComponent;
+
+@Component
+public class FirstServiceRouter extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		ElasticsearchComponent elasticsearchComponent = new ElasticsearchComponent();
+		elasticsearchComponent.setHostAddresses("localhost:9200");
+
+		getContext().addComponent("elasticsearch", elasticsearchComponent);
+		rest("/v1/consumers/id-verification/nafath-app/initiate").post().to("direct:firstService");
+		from("direct:firstService")
+				.log("body: ${body}")
+				.process(new BuildBodyProcessor())
+				.to("elasticsearch://elasticsearch?operation=Index&indexName=twitter");
+
+	}
+
+}
+
+class BuildBodyProcessor implements Processor {
+
+	@Override
+	public void process(org.apache.camel.Exchange exchange) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("user", "hi");
+		map.put("postDate", "2021-07-01");
+		map.put("message", exchange.getIn().getBody(String.class));
+		exchange.getIn().setBody(map);
+	}
+}
